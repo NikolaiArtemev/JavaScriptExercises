0e1156b6d8363f5343bcd673f50bf78835c9514e
Nikolai <nikolai_artemev@epam.com>
2016-10-27 19:12:15 +0200

    base tasks 4-10

---
+++ task_10.js
@@ -0,0 +1,10 @@
+        if(browser === 'IE'){
+            console.log('Please, download Chrome or Firefox');
+        }
+        else if(browser === 'Chrome' 
+        || browser ===  'Firefox'
+        || browser ===  'Safari'
+        || browser ===  'Opera'){
+            console.log('This browser is supported!');
+        }
+        else console.log('We are not able to identify your browser');
